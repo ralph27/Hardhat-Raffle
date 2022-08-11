aa3fa991d29504a02ed9685351d1b8a82fbc7799
ralph27 <maaloulyralph@gmail.com>
2022-08-11 20:31:59 +0300

    cleaning folders

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,14 +1,13 @@
 const { getNamedAccounts, deployments, network, ethers } = require("hardhat")
 
-const BASE_FEE = "250000000000000000" // 0.25 is this the premium in LINK?
-const GAS_PRICE_LINK = 1e9 // link per gas, is this the gas lane? // 0.000000001 LINK per gas
+const BASE_FEE = "250000000000000000"
+const GAS_PRICE_LINK = 1e9 
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
-    // If we are on a local development network, we need to deploy mocks!
     if (chainId == 31337) {
         log("Local network detected! Deploying mocks...")
         await deploy("VRFCoordinatorV2Mock", {
